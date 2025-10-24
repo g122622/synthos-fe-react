7f24aa2976ce7907563730c52a14bd222c6bd8c9
g122622 <guoyi122622@gmail.com>
2025-10-24 20:49:54 +0800

    fix：修复低视口高度下双滚动条的问题

--- src/pages/latest-topics.tsx
+++ src/pages/latest-topics.tsx
@@ -527,7 +527,7 @@ export default function LatestTopicsPage() {
                             </div>
                         ) : currentTopics.length > 0 ? (
                             <div className="flex flex-col gap-4">
-                                <ScrollShadow className="max-h-[900px]">
+                                <ScrollShadow className="max-h-[700px]">
                                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-5">
                                         {currentTopics.map((topic, index) => {
                                             // 解析参与者
