3ef65710974dc28f0059119d2936234d6ba3558f
g122622 <guoyi122622@gmail.com>
2025-11-12 21:43:21 +0800

    fxi：按扭颜色使用主题色

--- src/components/ResponsivePopover.tsx
+++ src/components/ResponsivePopover.tsx
@@ -37,7 +37,7 @@ const ResponsivePopover: React.FC<ResponsivePopoverProps> = ({ buttonText, child
     return (
         <Popover placement="bottom">
             <PopoverTrigger>
-                <Button>{buttonText}</Button>
+                <Button color="primary">{buttonText}</Button>
             </PopoverTrigger>
             <PopoverContent>{children}</PopoverContent>
         </Popover>
